0af61e587c93b3e18ff5fa287ec20604bdae5a12
Denidu Gamage <denidugamage21@gmail.com>
2025-05-21 12:01:59 +0530

    Initial Commit

---
+++ SwiftUIGuardApp/SwiftUIGuardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGuardAppApp.swift
+//  SwiftUIGuardApp
+//
+//  Created by Denidu Gamage on 2025-05-21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGuardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIGuardAppTests/SwiftUIGuardAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGuardAppTests.swift
+//  SwiftUIGuardAppTests
+//
+//  Created by Denidu Gamage on 2025-05-21.
+//
+
+import Testing
+@testable import SwiftUIGuardApp
+
+struct SwiftUIGuardAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
